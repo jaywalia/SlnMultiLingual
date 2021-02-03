fb296c390810e7aea8d898c3ee83520bbcebe5a9
Jay Walia <jwalia@uh.edu>
2021-02-03 14:16:32 -0600

    Add project files.

---
+++ Calc.UI.Cnsl/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Calc.UI.Cnsl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator in C# ");
+            //Console.WriteLine(Calculator.Simple.Add(2, 3));
+            //TestOverflow();
+            //CatchOverflow();
+            //MaybeThisTimeWeCanCatchIt();
+            SetTheNameRight();
+        }
+
+        static void TestOverflow()
+        {
+            int x = int.MaxValue;
+            Console.WriteLine(Calculator.Simple.Add(x, 1));
+        }
+
+        static void CatchOverflow()
+        {
+            checked
+            {
+                try
+                {
+                    Console.WriteLine(Calculator.Simple.Add(int.MaxValue, 1));
+                }
+                catch (OverflowException)
+                {
+                    throw;
+                }
+            }
+        }
+
+        static void MaybeThisTimeWeCanCatchIt()
+        {
+            Console.WriteLine(Calculator.Simple.SafeAdd(int.MaxValue, 3));
+        }
+
+        static void SetTheNameRight()
+        {
+            string name = "jay";
+            Console.WriteLine(name);
+            string fixedName = name.Capitalize();
+            Console.WriteLine(fixedName);
+        }
+    }
+
+    static class StringExtensions
+    {
+        public static string Capitalize(this string input)
+        {
+            return input.Substring(0, 1).ToUpper() +
+                    input.Substring(1);
+        }
+    }
+}
---
+++ Calculator/Simple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Simple
+    {
+        public static int Add(int x, int y) => x + y;
+        public static int Sub(int x, int y) => x - y;
+        public static int Mul(int x, int y) => x * y;
+        public static int Div(int x, int y) => x / y;
+
+        public static int SafeAdd(int x, int y)
+        {
+            checked 
+            {
+                return x + y;
+            }
+        }
+    }
+}
